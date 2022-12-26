986430e05844b8ac36a67e2e5637e046a668d178
Chase Skibeness <chase.skibeness@limeade.com>
2022-12-26 15:40:35 -0800

    add search input

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { agent } from "superagent";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const Banner = ({ onTitleSearch }) => {
+
+  handleSearch = (event) => {
+    const searchString = event.target.value;
+
+    if (searchString >= 3) {
+      onTitleSearch(
+        searchString,
+        (page) => agent.Items.byTitle(searchString, page),
+        agent.Items.byTitle(searchString)
+      );
+    }
+  }
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -9,6 +23,7 @@ const Banner = () => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
+          <input type="search" onChange={handleSearch}></input>
           <span> the cool stuff.</span>
         </div>
       </div>
