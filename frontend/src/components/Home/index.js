5884a4896a0c166d9a260235698cc57e62567c6c
Chase Skibeness <chase.skibeness@limeade.com>
2022-12-26 15:42:57 -0800

    add reducer and dispatch

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -8,6 +8,7 @@ import {
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
   APPLY_TAG_FILTER,
+  APPLY_TITLE_FILTER,
 } from "../../constants/actionTypes";
 
 const Promise = global.Promise;
@@ -21,6 +22,8 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   onClickTag: (tag, pager, payload) =>
     dispatch({ type: APPLY_TAG_FILTER, tag, pager, payload }),
+  onTitleSearch: (searchString, pager, payload) =>
+    dispatch({ type: APPLY_TITLE_FILTER, searchString, pager, payload }),
   onLoad: (tab, pager, payload) =>
     dispatch({ type: HOME_PAGE_LOADED, tab, pager, payload }),
   onUnload: () => dispatch({ type: HOME_PAGE_UNLOADED }),
@@ -45,8 +48,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home-page">
-        <Banner />
-
+        <Banner onTitleSearch={this.props.onTitleSearch} />
         <div className="container page">
           <Tags tags={this.props.tags} onClickTag={this.props.onClickTag} />
           <MainView />
--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -10,6 +10,7 @@ import {
   PROFILE_PAGE_UNLOADED,
   PROFILE_FAVORITES_PAGE_LOADED,
   PROFILE_FAVORITES_PAGE_UNLOADED,
+  APPLY_TITLE_FILTER,
 } from "../constants/actionTypes";
 
 const reducer = (state = {}, action) => {
@@ -37,6 +38,16 @@ const reducer = (state = {}, action) => {
         currentPage: action.page,
       };
     case APPLY_TAG_FILTER:
+      return {
+        ...state,
+        pager: action.pager,
+        items: action.payload.items,
+        itemsCount: action.payload.itemsCount,
+        tab: null,
+        tag: action.tag,
+        currentPage: 0,
+      };
+    case APPLY_TITLE_FILTER:
       return {
         ...state,
         pager: action.pager,
