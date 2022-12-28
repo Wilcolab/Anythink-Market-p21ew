adb9f573826ce79fb2665a27b4a88bb526824825
Chase Skibeness <chase.skibeness@limeade.com>
2022-12-28 00:05:03 -0800

    styling

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -25,11 +25,20 @@ const Banner = ({ onTitleSearch }) => {
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <input type="search" onChange={handleSearch}></input>
-          <span> the cool stuff.</span>
+        <div className="row align-items-center justify-content-center">
+          <div className="col-2">
+            <span className="col-form-label">A place to </span>
+            <span id="get-part" className="col-form-label">get</span>
+          </div>
+          <div className="col-5 input-group">
+            <input type="search" className="form-control py-2 border-right-0 border" placeholder="What is it that you truly desire?" onChange={handleSearch}></input>
+            <span className="input-group-append bg-transparent">
+              <i class="bi bi-search input-group-text bg-white text-primary"></i>
+            </span>
+          </div>
+          <div className="col-2">
+            <span className="col-form-label"> the cool stuff.</span>
+          </div>
         </div>
       </div>
     </div>
